b6ccede57262525a7e2317c2b774465158ac4c3f
Helio <heliosoto17@gmail.com>
2021-12-02 13:19:04 +0100

    new component dropzone

---
+++ src/components/Dropzone.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Dropzone = () => {
+    return (
+        <div>
+            funciono
+        </div>
+    )
+}
--- src/components/screen/Layout.jsx
+++ src/components/screen/Layout.jsx
@@ -2,12 +2,13 @@
 import React from 'react'
 //styles
 import '../../styles/layout.css'
+import { Dropzone } from '../Dropzone'
 export const Layout = () => {
     return (
         <div className="main">
             <h1 className="main__title">DROPZONE VIEW</h1>
             <div className="main__center">
-                <h3 className="main__center--title">DROPZONE AREA</h3>
+                <Dropzone />
                 <div></div>
                 <div className="center__rigth">
                     <div className="center__rigth--item">Calcular resultados</div>
